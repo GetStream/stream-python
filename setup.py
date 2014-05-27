ef4ed22698631e927117d3f8f6653cf2b562faac
Thierry Schellenbach <thierryschellenbach@gmail.com>
2014-05-27 16:24:07 +0300

    added support for python setup.py test

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class PyTest(TestCommand):
 
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = []
+        self.test_args = ['stream/tests.py']
         self.test_suite = True
 
     def run_tests(self):
