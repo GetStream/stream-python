6b3ae8c0e4a5882778acdc750c99b89fb0fe71aa
Thierry Schellenbach <thierryschellenbach@gmail.com>
2014-05-28 18:47:58 +0200

    tests are running

--- setup.py
+++ setup.py
@@ -26,14 +26,12 @@ class PyTest(TestCommand):
 
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        
-        self.test_args = ['stream/tests.py --cov stream --cov-report term-missing']
         self.test_suite = True
 
     def run_tests(self):
         # import here, cause outside the eggs aren't loaded
         import pytest
-        errno = pytest.main(self.test_args)
+        errno = pytest.main('stream/tests.py --cov stream --cov-report term-missing')
         sys.exit(errno)
 
 setup(
