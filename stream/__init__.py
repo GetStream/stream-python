97cdd904eae6b6de978a412a699ba11b7b5ba726
Thierry Schellenbach <thierryschellenbach@gmail.com>
2014-05-28 11:42:06 +0200

    pep8

--- stream/__init__.py
+++ stream/__init__.py
@@ -3,7 +3,7 @@ __author__ = 'Thierry Schellenbach'
 __copyright__ = 'Copyright 2012, Thierry Schellenbach'
 __credits__ = ['Thierry Schellenbach, mellowmorning.com, @tschellenbach']
 __license__ = 'BSD'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __maintainer__ = 'Thierry Schellenbach'
 __email__ = 'thierryschellenbach@gmail.com'
 __status__ = 'Production'
@@ -23,9 +23,6 @@ def connect(api_key, api_secret):
 '''
 TODO:
 - Api & Code review Tommaso
-
-- follow sometimes breaks
-- missing field exceptions are raised in an inconsistent way to other errors
 
 - test coverage
 - read the docs
--- stream/signing.py
+++ stream/signing.py
@@ -22,5 +22,3 @@ def sign(secret, value):
     signed = hmac.new(key, msg=str(value), digestmod=hashlib.sha1)
     signature = b64_encode(signed.digest())
     return str(signature)
-
-
