2fa656d1ac369c16d8a0c41bf725e74d0e3c77d1
Jelte Fennema <github-tech@jeltef.nl>
2016-10-11 18:58:38 +0200

    Tests: Make _test_sleep a dummy function

--- stream/tests.py
+++ stream/tests.py
@@ -84,10 +84,7 @@ class ClientTest(TestCase):
         :param local_wait: float, number of seconds to sleep when hitting localhost API
         :return: None
         """
-        sleep_time = production_wait
-        if self.local_tests:
-            sleep_time = local_wait
-        time.sleep(sleep_time)
+        return
 
     def test_update_activities_create(self):
         activities = [{
