f97f36430e2f05deef9c91adacb5d8a70a84d8dc
Jelte Fennema <github-tech@jeltef.nl>
2016-10-18 09:37:45 +0200

    setup.py: Capitalize url

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ Documentation
 -------------
 Full documentation is available on `Github`_.
 
-.. _`Github`: https://github.com/getstream/stream-python
+.. _`Github`: https://github.com/GetStream/stream-python
 '''
 
 install_requires = [
